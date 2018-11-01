46766ef6d9a91d7be6cb5c04877d46ddd77b5df1
MohammedAlnajeh <43445141+MohammedAlnajeh@users.noreply.github.com>
2018-11-01 16:45:32 -0400

    eddited pizza order

--- PizzaOrderMo/PizzaOrderMo/PizzaOrderForm.cs
+++ PizzaOrderMo/PizzaOrderMo/PizzaOrderForm.cs
@@ -20,24 +20,27 @@ namespace PizzaOrderMo
         private void btnCalculate_Click(object sender, EventArgs e)
         {
             //declare local variables
-            double  subtotal, tax, total, sizeselections, topping;
+            double subtotal, tax, total, sizeSelections, toppingSelection;
 
-            const double LARGE = 9.99;
-            const double XLARGE = 12.99;
+
+            const double XLARGE = 2;
+            const double LARGE = 1;
+            const double LARGEPRICE = 9.99;
+            const double XLARGEPRICE = 12.99;
             const double FIRSTTOPPING = 0.75;
             const double SECONDTOPPING = 1.35;
             const double THIRDTOPPING = 2.15;
             const double FOURTHTOPPING = 2.75;
-            
 
 
-           sizeselections= (double)nudSizeNum.Value;
 
 
-            topping= (double)nudToppingNum.Value;
+            sizeSelections = (double)nudSizeNum.Value;
+            toppingSelection = (double)nudToppingNum.Value;
 
-            subtotal = sizeselections + topping;
-           total = subtotal * 0.13 + subtotal;
+
+            subtotal = sizeSelections + toppingSelection;
+            total = subtotal * 0.13 + subtotal;
             tax = subtotal * 0.13;
 
             lblSubtotalAnswer.Text = string.Format("${0:0.00}", subtotal);
@@ -45,25 +48,80 @@ namespace PizzaOrderMo
             lblTotalAnswer.Text = string.Format("${0:0.00}", total);
 
 
+            if (sizeSelections == LARGE)
+            {
+                if (toppingSelection == FIRSTTOPPING)
+                {
+                    subtotal = LARGEPRICE + FIRSTTOPPING;
+                    total = subtotal * 0.13 + subtotal;
+                    tax = subtotal * 0.13;
+                }
+                else if (toppingSelection == SECONDTOPPING)
+                {
+                    subtotal = LARGEPRICE + SECONDTOPPING;
+                    total = subtotal * 0.13 + subtotal;
+                    tax = subtotal * 0.13;
+                }
+                else if (toppingSelection == THIRDTOPPING)
+                {
+                    subtotal = LARGEPRICE + THIRDTOPPING;
+                    total = subtotal * 0.13 + subtotal;
+                    tax = subtotal * 0.13;
+                }
+                else if (toppingSelection == FOURTHTOPPING)
+                {
+                    subtotal = LARGEPRICE + FOURTHTOPPING;
+                    total = subtotal * 0.13 + subtotal;
+                    total = subtotal * 0.13;
+                }
+                if (sizeSelections == XLARGE)
+                {
+                    if (toppingSelection == FIRSTTOPPING)
+                    {
+                        subtotal = XLARGEPRICE + FIRSTTOPPING;
+                        total = subtotal * 0.13 + subtotal;
+                    tax = subtotal * 0.13;
+                    }
+                    
+                    }
+                    else if (toppingSelection == SECONDTOPPING)
+                    {
+                        subtotal = XLARGEPRICE + SECONDTOPPING;
+                        total = subtotal * 0.13 + subtotal;
+                        tax = subtotal * 0.13;
+                    }
+                    else if (toppingSelection == THIRDTOPPING)
+                    {
+                        subtotal = XLARGEPRICE + THIRDTOPPING;
+                        total = subtotal * 0.13 + subtotal;
+                        tax = subtotal * 0.13;
+                    }
+                    else if (toppingSelection == FOURTHTOPPING)
+                    {
+                    subtotal = XLARGEPRICE + FOURTHTOPPING;
+                    total = subtotal * 0.13 + subtotal;
+                    tax = subtotal * 0.13;
+                    }
+                
 
 
+
+
+                
+
+            }
+
+        }
+
+        private void frmPizzaOrder_Load(object sender, EventArgs e)
+        {
             lblSubtotal.Show();
             lblTotal.Show();
             lblHST.Show();
             lblHSTAnswer.Show();
             lblSubtotalAnswer.Show();
             lblTotalAnswer.Show();
-
-        }
-
-        private void frmPizzaOrder_Load(object sender, EventArgs e)
-        {
-            lblSubtotal.Hide();
-            lblTotal.Hide();
-            lblHST.Hide();
-            lblHSTAnswer.Hide();
-            lblSubtotalAnswer.Hide();
-            lblTotalAnswer.Hide();
         }
     }
+
 }
